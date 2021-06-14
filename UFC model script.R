c04c20777b8bf836a078c5b8ba8788d04f801a41
baljor3 <baljor3@cs.sfu.ca>
2021-06-14 15:44:58 -0700

    got links from fighter page

--- UFC model script.R
+++ UFC model script.R
@@ -1,10 +1,28 @@
 install.packages("rvest")
+install.packages("dplyr")
+install.packages("stringr")
 
 library(rvest)
+library(dplyr)
+library(stringr)
 
 URLUFCStats <- read_html("http://ufcstats.com/statistics/fighters")
 
 URLtostats<-URLUFCStats%>%
   html_nodes(".b-link")%>%
   html_attrs()
-  
+
+for( i in 1:length(URLtostats)){
+  URLtostats[[i]]<-URLtostats[[i]][1]
+}
+
+Fighterstatspage<-read_html(as.vector(unique(URLtostats)[[1]]))
+
+#TODO: figure how to get stats from website
+Fighterstatspage%>%
+  html_nodes("li")
+
+
+c<-Fighterstatspage%>%
+  html_nodes("[class='b-list__box-list-item b-list__box-list-item_type_block']")
+str_subset(c,'[:digit:]')
