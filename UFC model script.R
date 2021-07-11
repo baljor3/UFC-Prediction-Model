31d18b17ee5706579b064ca515b04a9b5287bf11
bobby <baljor20@gmail.com>
2021-07-10 17:08:28 -0700

    scraping with selenium

--- UFC model script.R
+++ UFC model script.R
@@ -16,101 +16,18 @@ library(stringr)
 library(RSelenium)
 
 
-# data scraping from third party UFC site ---------------------------------
-
-
-URLUFCStats <- read_html("http://ufcstats.com/statistics/fighters?char=a&page=all")
-
-URLtostats<-URLUFCStats%>%
-  html_nodes(".b-link")%>%
-  html_attrs()
-
-for( i in 1:length(URLtostats)){
-  URLtostats[[i]]<-URLtostats[[i]][1]
-}
-URLtostats<-unique(URLtostats)
-
-Databaseoffighters<-data.frame(height=NA,reach=NA,stance=NA,slpm=NA,stracc=NA,sapm=NA,strdef=NA,tdavg=NA,
-                               tdacc=NA, tddef=NA, subavg=NA,wins=NA,losses=NA,lastfight=NA)
-
-ischarempty<-character(0)
-#TODO:forgot to scrape stances 
-for(i in 1:length(URLtostats)){
-Fighterstatspage<-read_html(URLtostats[[i]])
-
-#Putting data into a data frame
-StatsofFighter<-Fighterstatspage%>%
-  html_nodes("li")
-StatsofFighter<-str_replace_all(StatsofFighter,'<.*?>', "")
-StatsofFighter<-str_replace_all(StatsofFighter,'\n', "")
-StatsofFighter<-str_subset(StatsofFighter,"[:digit:]")
-StatsofFighter<-trimws(StatsofFighter)
-StatsofFighter<-str_remove_all(StatsofFighter,"[:punct:]")
-StatsofFighter<-str_remove(StatsofFighter," ")
-
-Lastfight <-Fighterstatspage%>%
-  html_nodes("[class='b-flag__text']")
-Lastfight<-Lastfight[1]
-Lastfight<-str_replace_all(Lastfight,'<.*?>',"")
-
-if(identical(Lastfight,ischarempty)){
-  Lastfight<-NA
-}
-
-Record<-Fighterstatspage%>%
-  html_nodes("span")
-Record<-Record[2]
-Record<-str_remove_all(Record,"<.*?>")
-Record<-str_remove_all(Record,"\n")
-Record<-trimws(Record)
-Record<-str_remove_all(Record,"[:alpha:]")
-Record<-str_remove_all(Record,"\\:")
-Record<-str_remove_all(Record," ")
-Wins<-str_extract(Record,"[:digit:].?")
-Wins<-str_remove(Wins,"-")
-Losses<-str_extract(Record,"-[:digit:].?")
-Losses<-str_remove_all(Losses,"-")
-
-nameofstats<-str_replace_all(StatsofFighter,"[:digit:]","")
-nameofstats<-str_replace_all(nameofstats," ","")
-nameofstats<-tolower(nameofstats)
-
-StatsofFighter<-str_replace_all(StatsofFighter,"[:alpha:]","")
-StatsofFighter<-str_replace_all(StatsofFighter," ","")
-
-Listofstatsandnames<-vector(mode="list",length = 2)
-Listofstatsandnames[[1]]<-nameofstats
-Listofstatsandnames[[2]]<-StatsofFighter
-
-Rowtobeadded<-rep(NA,length(Databaseoffighters))
-
-Rowtobeadded[12]<-Wins
-Rowtobeadded[13]<-Losses
-Rowtobeadded[14]<-Lastfight
-
-for(j in 1:length(Listofstatsandnames[[1]])){
-  if(Listofstatsandnames[[1]][j] %in% names(Databaseoffighters)){
-    Rowtobeadded[which(names(Databaseoffighters) %in% Listofstatsandnames[[1]][j])]<-Listofstatsandnames[[2]][j]
-  }
-}
-Databaseoffighters<-rbind(Databaseoffighters,Rowtobeadded)
-}
-#Databaseoffighters<-Databaseoffighters[-1,]
-
-
 # UFC official website scraping -------------------------------------------
 
 # the number of fighers is 1019 the number of fighers per load is 11. Hence,
 # (1019-11)/11 = 92 this is the total number of times we should load.
 
-for(i in 1:92){
-#TODO:figure out Selenium
 # Download binaries, start driver, and get client object.
-rd <- rsDriver(browser = "firefox", port = 4414L)
+rd <- rsDriver(browser = "firefox", port = 4444L)
 ffd <- rd$client
 
 # Navigate to page.
 ffd$navigate("https://www.ufc.com/athletes/all")
+for(i in 1:92){
 
 # Find the load button and assign, then send click event.
 load_btn <- ffd$findElement(using = "class name", "pager__item")
@@ -119,11 +36,15 @@ load_btn$clickElement()
 # Wait for elements to load.
 Sys.sleep(2)
 }
-URLUFCStats <- read_html("https://www.ufc.com/athletes/all")
+
+#to get the current page with all the loaded fighters.
+URLUFCStats<-read_html(ffd$getPageSource()[[1]])
+
 
 URLtostats<-URLUFCStats%>%
   html_nodes(".e-button--black")%>%
   html_attrs()
+
 
 for( i in 1:length(URLtostats)){
   URLtostats[[i]]<-URLtostats[[i]][1]
@@ -135,10 +56,19 @@ for(i in 1:length(URLtostats)){
   URLtostats[[i]]<-paste(UFCLink,URLtostats[[i]],sep="")
 }
 
-Fighterstatspage<-read_html(URLtostats[[1]])
+ffd$navigate(URLtostats[[1]])
+
+load_btn <- ffd$findElement(using = "class name", "e-button--white")
+load_btn$clickElement()
+
+Sys.sleep(2)
+
+Fighterstatspage<-read_html(ffd$getPageSource()[[1]])
+
 Fighterstatspage%>%
   html_nodes(".c-record__promoted")%>%
-  str_replace_all("<.*?>","")
+  str_replace_all("<.*?>","")%>%
+  str_remove_all("\n")
 
 Fighterstatspage%>%
   html_nodes(".e-chart-circle")%>%
